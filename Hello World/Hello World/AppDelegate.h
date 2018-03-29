ceb9751591bc17c6872b2ca79072aa2506ec7038
Colin Russell <colinrussell101@gmail.com>
2018-03-29 12:19:20 -0400

    update to LargestNumberInAnArray

---
+++ Hello World/Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Colin on 2018-03-23.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
